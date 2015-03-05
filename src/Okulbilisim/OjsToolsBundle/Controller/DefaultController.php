40ae91137cf34b694f81db04469e7a2d90bdd99b
Hasan Tayyar BESIK <tayyar.besik@gmail.com>
2015-03-05 14:50:11 +0200

    data and command added

--- src/Okulbilisim/OjsToolsBundle/Controller/DefaultController.php
+++ src/Okulbilisim/OjsToolsBundle/Controller/DefaultController.php
@@ -1,13 +1,26 @@
 <?php
 
-namespace Okulbilisim\OjsToolsBundle\Controller;
+namespace Ojs\CliBundle\Command;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use \Symfony\Component\Console\Input\StringInput;
 
-class DefaultController extends Controller
-{
-    public function indexAction()
-    {
-        return $this->render('OkulbilisimOjsToolsBundle:Default:index.html.twig');
+class DataToolsCommand extends ContainerAwareCommand {
+
+    protected function configure() {
+        $this
+                ->setName('ojs:data:install')
+                ->setDescription('Generate and save data for a specific  ');
     }
+
+    protected function execute(InputInterface $input, OutputInterface $output) {
+        $kernel = $this->getContainer()->get('kernel');
+        $application = new \Symfony\Bundle\FrameworkBundle\Console\Application($kernel); 
+        $output->writeln('<info>Adding  data</info>'); 
+        
+        $output->writeln("\nDONE\n");
+    }
+
 }
