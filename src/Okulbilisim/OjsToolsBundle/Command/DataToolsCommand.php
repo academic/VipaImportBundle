9516f81a6be7540d5385284c358e4cd05ea6dec5
Hasan Tayyar BESIK <tayyar.besik@gmail.com>
2015-03-05 16:27:51 +0200

    fixtures and command updated

--- src/Okulbilisim/OjsToolsBundle/Command/DataToolsCommand.php
+++ src/Okulbilisim/OjsToolsBundle/Command/DataToolsCommand.php
@@ -6,22 +6,34 @@ use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use \Symfony\Component\Console\Input\StringInput;
+use Symfony\Component\Console\Input\InputArgument;
 
 class DataToolsCommand extends ContainerAwareCommand {
 
     protected function configure() {
         $this
                 ->setName('ojs:install:data')
-                ->setDescription('Add data');
+                ->setDescription('Add data')
+                ->addArgument(
+                        'instutionCode', InputArgument::REQUIRED, 'Instution code and also folder name under DataFixtures/');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output) {
         $kernel = $this->getContainer()->get('kernel');
         $application = new \Symfony\Bundle\FrameworkBundle\Console\Application($kernel);
         $application->setAutoExit(false);
-
         $output->writeln('<info>Adding  data</info>');
 
+
+        $files = [];
+        while (false !== ($entry = readdir(__DIR__ . '/../DataFixtures/Alice/' . $input->getArgument('instutionCode') . '/'))) {
+            $files[] = "$entry\n";
+        }
+        /*
+          $manager = $this->get('h4cc_alice_fixtures.manager');
+          $objects = $manager->loadFiles($files, 'yaml');
+          $manager->persist($objects, true);
+         */
         $output->writeln("\nDONE\n");
     }
 
