b4e872033604ca030bd8e9609e3d2bcbfab7ad96
Utku Aydın <utkuaydin34@gmail.com>
2015-10-22 17:40:49 +0300

    Create a command for importing journals using multiple IDs

---
+++ src/OkulBilisim/OjsImportBundle/Importer/PKP/GivenJournalsImporter.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace OkulBilisim\OjsImportBundle\Importer\PKP;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\ORM\EntityManager;
+use Exception;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class GivenJournalsImporter extends Importer
+{
+    /** @var UserImporter */
+    private $ui;
+
+    /**
+     * AllJournalsImporter constructor.
+     * @param Connection $connection
+     * @param EntityManager $em
+     * @param LoggerInterface $logger
+     * @param OutputInterface $consoleOutput
+     * @param UserImporter $ui
+     */
+    public function __construct(
+        Connection $connection,
+        EntityManager $em,
+        LoggerInterface $logger,
+        OutputInterface $consoleOutput,
+        UserImporter $ui
+    )
+    {
+        parent::__construct($connection, $em, $logger, $consoleOutput);
+        $this->ui = $ui;
+    }
+
+    public function importJournals($ids)
+    {
+        $journalsSql = "SELECT journal_id, path FROM journals WHERE journal_id IN (:ids)";
+        $journalsStatement = $this->connection->prepare($journalsSql);
+        $journalsStatement->bindValue('ids', implode(', ', $ids));
+        $journalsStatement->execute();
+        $journals = $journalsStatement->fetchAll();
+
+        $journalImporter = new JournalImporter(
+            $this->connection, $this->em, $this->logger, $this->consoleOutput, $this->ui
+        );
+
+        foreach ($journals as $journal) {
+            $existingJournal = $this->em
+                ->getRepository('OjsJournalBundle:Journal')
+                ->findOneBy(['slug' => $journal['path']]);
+
+            if (!$existingJournal) {
+                try {
+                    $ids = $journalImporter->importJournal($journal['journal_id']);
+                    $journalUserImporter = new JournalUserImporter($this->connection, $this->em, $this->logger, $this->consoleOutput);
+                    $journalUserImporter->importJournalUsers($ids['new'], $ids['old'], $this->ui);
+                } catch (Exception $exception) {
+                    $message = sprintf(
+                        '%s: %s (uncaught exception) at %s line %s',
+                        get_class($exception),
+                        $exception->getMessage(),
+                        $exception->getFile(),
+                        $exception->getLine()
+                    );
+
+                    $this->consoleOutput->writeln('Importing of journal #' . $journal['id'] . 'failed.');
+                    $this->consoleOutput->writeln($message);
+                    $this->consoleOutput->writeln($exception->getTraceAsString());
+                }
+            } else {
+                $this->consoleOutput->writeln('Journal #' . $journal['id'] . ' already imported. Skipped.');
+            }
+        }
+    }
+
+}
