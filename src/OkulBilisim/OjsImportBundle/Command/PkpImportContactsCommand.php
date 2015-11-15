29b12e2c5d8827ea95476378c689b66e35f06fb7
Utku Aydın <utkuaydin34@gmail.com>
2015-11-16 01:02:12 +0200

    Create a command for importing journal contact, closes okulbilisim/ojs#1306

---
+++ src/OkulBilisim/OjsImportBundle/Command/PkpImportContactsCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace OkulBilisim\OjsImportBundle\Command;
+
+use Ojs\CoreBundle\Helper\StringHelper;
+use OkulBilisim\OjsImportBundle\Helper\ImportCommand;
+use OkulBilisim\OjsImportBundle\Importer\PKP\JournalContactImporter;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Stopwatch\Stopwatch;
+
+class PkpImportContactsCommand extends ImportCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('ojs:import:pkp:contacts')
+            ->setDescription('Import journal contacts from PKP/OJS');
+
+        parent::configure();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        parent::execute($input, $output);
+        $importer = new JournalContactImporter($this->connection, $this->em,  $this->logger, $output);
+
+        $journalsSql = 'SELECT journal_id, path FROM journals';
+        $journalsStatement = $this->connection->executeQuery($journalsSql);
+        $journals = $journalsStatement->fetchAll();
+
+        $stopwatch = new Stopwatch();
+        $stopwatch->start('stats_import');
+
+        foreach ($journals as $journal) {
+            $existingJournal = $this->em
+                ->getRepository('OjsJournalBundle:Journal')
+                ->findOneBy(['slug' => $journal['path']]);
+
+            if($existingJournal !== null && $existingJournal->getJournalContacts()->isEmpty()) {
+                $importer->importContacts($existingJournal, $journal['journal_id']);
+                $this->em->persist($existingJournal);
+                $this->em->flush();
+            }
+        }
+
+        $event = $stopwatch->stop('stats_import');
+        $output->writeln('Duration: ' . StringHelper::formatMilliseconds($event->getDuration()));
+    }
+}
---
+++ src/OkulBilisim/OjsImportBundle/Importer/PKP/JournalContactImporter.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace OkulBilisim\OjsImportBundle\Importer\PKP;
+
+use Ojs\JournalBundle\Entity\Journal;
+use Ojs\JournalBundle\Entity\JournalContact;
+use OkulBilisim\OjsImportBundle\Importer\Importer;
+
+class JournalContactImporter extends Importer
+{
+    /**
+     * @param Journal $journal
+     * @param int $journalId
+     * @throws \Doctrine\DBAL\DBALException
+     */
+    public function importContacts($journal, $journalId)
+    {
+        $this->consoleOutput->writeln("Importing journal's contacts...");
+
+        $settingsSql = "SELECT locale, setting_name, setting_value FROM journal_settings WHERE journal_id = :id";
+        $settingsStatement = $this->connection->prepare($settingsSql);
+        $settingsStatement->bindValue('id', $journalId);
+        $settingsStatement->execute();
+
+        $settings = array();
+        $pkpSettings = $settingsStatement->fetchAll();
+
+        foreach ($pkpSettings as $setting) {
+            $name = $setting['setting_name'];
+            $value = $setting['setting_value'];
+            $settings[$name] = $value;
+        }
+
+        $contact = new JournalContact();
+        $contact->setFullName($settings['contactName']);
+        $contact->setEmail($settings['contactEmail']);
+        $contact->setPhone($settings['contactPhone']);
+        $contact->setAddress($settings['contactMailingAddress']);
+
+        $types = $this->em->getRepository('OjsJournalBundle:ContactTypes')->findAll();
+        !empty($types) && $contact->setContactType($types[0]);
+
+        $journal->addJournalContact($contact);
+    }
+}
