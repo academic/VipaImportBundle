1dabc3508e801c33b77bc5fe0cfc4b59b0642b7d
Utku Aydın <utkuaydin34@gmail.com>
2016-04-03 12:06:46 +0300

    Allow disabling of Loggable extension

--- Helper/ImportCommand.php
+++ Helper/ImportCommand.php
@@ -4,12 +4,14 @@ namespace Ojs\ImportBundle\Helper;
 
 use Doctrine\DBAL\Connection;
 use Doctrine\ORM\EntityManager;
+use Gedmo\Loggable\LoggableListener;
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class ImportCommand extends ContainerAwareCommand
@@ -36,7 +38,8 @@ class ImportCommand extends ContainerAwareCommand
             ->addArgument('username', InputArgument::REQUIRED, 'Username for PKP/OJS database server')
             ->addArgument('password', InputArgument::REQUIRED, 'Password for PKP/OJS database server')
             ->addArgument('database', InputArgument::REQUIRED, 'Name of PKP/OJS database')
-            ->addArgument('driver', InputArgument::OPTIONAL, 'Database driver', 'pdo_mysql');
+            ->addArgument('driver', InputArgument::OPTIONAL, 'Database driver', 'pdo_mysql')
+            ->addOption('disable-logging', null, InputOption::VALUE_NONE, 'Disable logging of entity creations');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -60,5 +63,30 @@ class ImportCommand extends ContainerAwareCommand
         $path = $this->getContainer()->getParameter('kernel.root_dir') . '/logs/import.log';
         $this->logger = $this->getContainer()->get('logger');
         $this->logger->pushHandler(new StreamHandler($path, Logger::INFO));
+
+        if ($input->getOption('disable-logging')) {
+            $this->disableLoggableExtension();
+        }
+    }
+
+    protected function disableLoggableExtension()
+    {
+        $instance = null;
+
+        foreach ($this->em->getEventManager()->getListeners() as $event => $listeners) {
+            foreach ($listeners as $hash => $listener) {
+                if ($listener instanceof LoggableListener) {
+                    $instance = $listener;
+                    break 2;
+                }
+            }
+        }
+
+        if ($instance) {
+            $evm = $this->em->getEventManager();
+            $evm->removeEventListener(['onFlush'], $instance);
+            $evm->removeEventListener(['postPersist'], $instance);
+            $evm->removeEventListener(['loadClassMetadata'], $instance);
+        }
     }
 }
