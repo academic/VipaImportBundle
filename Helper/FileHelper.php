fc601cc7031a2a9b6f3039763e46e752e0cc84b4
Utku Aydın <utkuaydin34@gmail.com>
2016-11-30 21:46:43 +0300

    Add new extensions

--- Helper/FileHelper.php
+++ Helper/FileHelper.php
@@ -10,6 +10,9 @@ class FileHelper {
         'application/msword' => 'doc',
         'application/zip'    => 'zip',
         'application/xml'    => 'xml',
+        'text/plain'         => 'txt',
+        'application/rar '   => 'rar',
+        'application/x-rar'  => 'rar',
         'application/octet-stream' => 'bin',
         'application/text-plain:formatted' => 'txt',
         'application/vnd.openxmlformats-officedocument.wordprocessingml.document' => 'docx'
